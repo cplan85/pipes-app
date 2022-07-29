f1a60142034d6cced8f20b9adc53d2556aec3bd1
cplan85 <carlos_planchart@live.com>
2022-07-29 16:46:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  name: string = 'Carlos';
+  value: number = 230023;
+  obj = {
+    name: 'Carlos',
+  };
+  showName() {
+    console.log(this.name);
+  }
   title = 'pipesApp';
 }
