113c263912ff22e84146d16a38f245a96a919eab
cplan85 <carlos_planchart@live.com>
2022-08-04 18:26:39 +0200

    basic styling

--- src/app/sales/pages/basics/basics.component.ts
+++ src/app/sales/pages/basics/basics.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styleUrls: ['./basics.component.scss']
+  styleUrls: ['./basics.component.scss'],
 })
 export class BasicsComponent implements OnInit {
+  lowerName: string = 'carlos';
+  upperName: string = 'Carlos';
+  completeName: string = 'Carlos Planchart';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
